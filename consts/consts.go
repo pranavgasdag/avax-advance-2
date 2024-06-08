8f591b46671f633ecd7353d22f44ea31ce711b7a
pranavgasdag <pranavgadag18@gmail.com>
2024-06-08 23:13:16 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "metacraft"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "metachain"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "MT"
 )
 
 var ID ids.ID
